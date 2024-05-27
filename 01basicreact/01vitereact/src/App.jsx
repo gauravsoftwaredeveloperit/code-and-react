b437c694944b589bb734831e72468c42872300a6
gauravsoftwaredeveloperit <gauravkumarsingh2025@gmail.com>
2024-05-27 13:15:42 +0530

    complete 2 project in raect

--- 01basicreact/01vitereact/src/App.jsx
+++ 01basicreact/01vitereact/src/App.jsx
@@ -1,13 +1,16 @@
 
 import Chai from "./Chai"
 function App() {
+  const username ="chai and code"
 
 
   return (
     // <h1>code and react with vite | Gaurav kumar</h1>
+
+   
     <div>
     <Chai/>
-    <h1>chai and react</h1>
+    <h1>chai and react {username}</h1>
     <p>test para</p>
     </div>
    
--- 01basicreact/01vitereact/src/main.jsx
+++ 01basicreact/01vitereact/src/main.jsx
@@ -3,8 +3,46 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
 
+function MyApp()
+{
+    return(
+        <div>
+            <h1>custom App !</h1>
+        </div>
+    )
+}
+
+
+// const reactElement ={
+//     type: 'a',
+//     props:{
+//          href:'https://google.com',
+//          target:'_blank'
+//     },
+//     children: 'Click me to visit google'
+// }
+
+const AnotherElement =(
+    <a href='https://google.com' target='_blank'> vist google</a>
+)
+
+
+const anotherElement="chai and code"
+const reactElement = React.createElement(
+    'a',
+    {href:'https://google.com', target:'_blank',},
+      'click in the visit google',
+      anotherElement
+)
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+    // <App />
+    // <MyApp />
+     reactElement
+    // AnotherElement
+
  
 )
