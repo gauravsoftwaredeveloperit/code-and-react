100c3fc0bed933cfaa25e2c832e03d05e09ce968
gauravsoftwaredeveloperit <gauravkumarsingh2025@gmail.com>
2024-05-30 13:59:53 +0530

    create  05passwordGenerator project

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       className="w-full h-screen duration-200 "
       style={{ backgroundColor: color }}
     >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="fixed flex flex-wrap justify-center bottom-40 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
             onClick={() => setColor("Red")}
@@ -68,6 +68,7 @@ function App() {
             Pink
           </button>
           <button
+      
             onClick={() => setColor("Purple")}
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
             style={{ backgroundColor: "Purple" }}
