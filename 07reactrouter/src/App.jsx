154c4408a73a8094e71fb825290054e3c643b43d
gauravsoftwaredeveloperit <gauravkumarsingh2025@gmail.com>
2025-01-16 11:50:10 +0530

    context api discussion and error fixed 7 project

---
+++ 07reactrouter/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Home from './components/Home/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     {/* <Header  />
+    <Footer  />
+    <Home    />  */}
+    </>
+  )
+}
+
+export default App;
