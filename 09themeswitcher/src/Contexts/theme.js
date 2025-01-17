c5f3aa1a3d7b798f8b6cdfb2120690dcd98f64c2
gauravsoftwaredeveloperit <gauravkumarsingh2025@gmail.com>
2025-01-17 12:53:42 +0530

    Context Api project

---
+++ 09themeswitcher/src/Contexts/theme.js
@@ -0,0 +1,12 @@
+import { createContext,useContext } from "react";
+export const ThemeContext = createContext({
+    themeMode: 'dark',
+    darkTheme: {},
+    lightTheme: {}
+});
+
+export const ThemeProvider = ThemeContext.Provider;
+export default function useTheme() {
+    return useContext(ThemeContext);
+}
+
